dacfa4ad69fcb4b808aecf2f1470be3928f20fcc
Lyuobomyr Didyk <lyoubomyr.didyk@le-campus-numerique.fr>
2023-10-29 00:12:05 +0200

    added vimeo, lodash library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,28 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+const gallery = document.querySelector('.gallery');
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+           </a>
+        </li>`
+    )
+    .join('');
+}
+
+gallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+let showGallery = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Vimeo from '@vimeo/player'
+import throttle from 'lodash.throttle';
+  
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo(iframe);
+const STOR_KEY = "videoplayer-current-time";
+
+function getSavedTime() {
+  return localStorage.getItem(STOR_KEY);
+}
+
+const savedTime = getSavedTime();
+
+if (savedTime !== null) {
+  player.setCurrentTime(savedTime);
+}
+
+player.on('timeupdate', throttle(data => {
+  const currentTime = data.seconds;
+  localStorage.setItem(STOR_KEY, JSON.stringify(currentTime))
+}, 1000));
 
